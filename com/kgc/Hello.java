9dfc1be95ea874bc72f67a64fb4bde939aa9bca3
mufan <mufan@iu.cn>
2019-11-26 15:19:01 +0800

    123

---
+++ com/kgc/Hello.java
@@ -0,0 +1,10 @@
+package com.kgc;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("iu真可爱");
+        System.out.println("iu真可爱");
+        System.out.println("iu真可爱");
+    }
+
+}
